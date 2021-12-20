412d76f001e8eb57c64b526c486a840a5055b288
jmaya129 <89950080+jmaya129@users.noreply.github.com>
2021-12-20 12:23:37 -0500

    count button is working

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 import StateCard from "./Components/StateCard.js";
 import Nav from "./Components/Nav.js";
+import MyButton from "./Components/MyButton.js";
 
-function App() {
-  return (
-    <div className="App">
-      <Nav />
-      <StateCard />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  // declare function
+
+  render() {
+    return (
+      <div className="App">
+        <Nav />
+        <StateCard />
+        <MyButton />
+      </div>
+    );
+  }
 }
 
 export default App;
